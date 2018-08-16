cc43c8ab0a0e02081045b8b0ff5870f207f91e86
Leblanc Meneses <leblanc@robusthaven.com>
2018-08-16 02:32:25 -0500

    fetching ercot data

---
+++ functions/src/fetch.ts
@@ -0,0 +1,47 @@
+import * as Crawler from "crawler";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+export const hook = functions.https.onRequest(async (req, res) => {
+  try {
+    const data = await new Promise((resolve, reject) => {
+      const crawler = new Crawler({
+        maxConnections : 10,
+      });
+      
+      crawler.direct({
+        uri: 'http://www.ercot.com/content/cdr/html/hb_lz',
+        skipEventRequest: false,
+        callback: function(error, response) {
+          const $ = response.$;
+          if(error) {
+            reject(`${response.statusCode}: ${error}`);
+          } else {
+            const table = [];
+            $('.tableStyle > tbody > tr').each((index, row) => {
+              if(index <= 1) return;
+              const result = {};
+              $('td', row).each((columnIndex, column) => {
+                result[`column${columnIndex}`] = $(column).text().trim()
+              });
+              table.push(result);
+            });
+            resolve(table.reduce((agg, currentValue) => {
+              agg[currentValue['column0']] = currentValue;
+              return agg;
+            }, {}));
+          }
+        }
+      });
+    });
+
+    await admin.database().ref('/ercot')
+      .child(new Date().valueOf().toString())
+      .set(data);
+  } catch(ex) {
+    console.log(ex);
+    res.status(500).send(ex);
+    return;
+  }
+  res.status(200).send("");
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import  * as ercot from './fetch';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const ercotFetchData = ercot.hook;
